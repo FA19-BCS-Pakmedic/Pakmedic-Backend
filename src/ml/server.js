ad0a7deb7185d3ac0cf8bea9fcf1934f21b6d0ac
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-12-18 15:22:12 +0500

    Added Diabetic Retinopathy Prediction Module

--- src/ml/server.js
+++ src/ml/server.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const request = require("request");
 var fs = require("fs");
-const { json } = require("body-parser");
+var bodyParser = require("body-parser");
 
 app = express();
+
+app.use(bodyParser.json());
+
 const PORT = 3000;
 
-const encode = (data) => {
-  let buf = Buffer.from(data);
-  let base64 = buf.toString("base64");
-  return base64;
-};
-
-app.get("/home", async (req, res) => {
+app.get("/chestXray", async (req, res) => {
   await request(
     "http://127.0.0.1:5000/chestXray",
     function (error, response, body) {
@@ -25,20 +22,21 @@ app.get("/home", async (req, res) => {
   );
 });
 
+app.get("/retinopathy", async (req, res) => {
+  await request.get(
+    {
+      url: "http://127.0.0.1:5000/retinopathy",
+      body: req.body,
+      json: true,
+    },
+    function (error, response, body) {
+      res.send(body);
+    }
+  );
+});
+
 app.get("/", function (req, res) {
   res.send("Hello World");
-});
-
-app.get("/non-blocking", (req, res) => {
-  res.status(200).send("This page is non-blocking");
-});
-
-app.get("/blocking", async (req, res) => {
-  let counter = 0;
-  for (let i = 0; i < 20_000_000_000; i++) {
-    counter++;
-  }
-  res.status(200).send(`result is ${counter}`);
 });
 
 app.listen(PORT, function () {
--- src/ml/test.py
+++ src/ml/test.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask,request
 import sys
+import json
 
 app = Flask(__name__)
 
 sys.path.insert(0, r'utils')
 
 from utils import Xray_util as util
+from utils import Retinopathy_util as retinopathy
+
 import numpy as np
 
 IMAGE_DIR = "sample_ChestXray"
@@ -36,7 +39,7 @@ def flask():
 @app.route('/chestXray', methods=['GET'])
 def xray():
     # load model
-    model = util.load_model('ML-models/Xray_model', compile=False)
+    model = util.load_model('ML_models/Xray_model', compile=False)
 
     buffer = util.compute_gradcam(model, 'cardiomegaly.png', IMAGE_DIR, labels, to_show)
 
@@ -46,5 +49,21 @@ def xray():
 
     return buffer
 
+@app.route('/retinopathy', methods=['GET'])
+def Retinopathy():
+    json  = request.get_json() 
+
+    data = json['user']
+
+    res = retinopathy.predict(data)
+
+    if(res==1):
+        response = "You have been diagonsed with Retinopathy"
+    else : 
+        response = "You have not been diagonsed with Diabetic Retinopathy"
+
+    return response
+
+    
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
