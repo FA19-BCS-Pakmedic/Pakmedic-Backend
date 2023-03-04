96dd05178886876310910d2fb112d9b860a20154
Mohammad Haris Zia <mhzxbox360@gmail.com>
2023-03-04 17:09:17 +0500

    Added ML functionality

--- src/ml/server.js
+++ src/ml/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const request = require("request");
 var fs = require("fs");
 var bodyParser = require("body-parser");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
 app = express();
 
@@ -10,8 +12,8 @@ app.use(bodyParser.json());
 const PORT = 3000;
 
 app.get("/chestXray", async (req, res) => {
-  await request(
-    "http://127.0.0.1:5000/chestXray",
+  request(
+    "http://127.0.0.1:5000/chestXray" + "?" + req.query.name,
     function (error, response, body) {
       fs.writeFileSync("ML-Brilliance.png", body, "base64", function (err) {
         res.send(Error);
@@ -23,9 +25,43 @@ app.get("/chestXray", async (req, res) => {
 });
 
 app.get("/retinopathy", async (req, res) => {
-  await request.get(
+  //Values in the following order AGE, Systolic BP, Diastolic BP, Cholestrol Value
+  request.get(
     {
       url: "http://127.0.0.1:5000/retinopathy",
+      body: req.body,
+      json: true,
+    },
+    function (error, response, body) {
+      res.send(body);
+    }
+  );
+});
+
+app.get("/riskOfDeath", async (req, res) => {
+  //Values to be sent in the body
+  // ValuesSequence = ["Age",
+  //                 "DiastolicBP",
+  //                 "Povertyindex",
+  //                 "Race",
+  //                 "RedBloodcells",
+  //                 "SedimentationRate",
+  //                 "SerumAlbumin",
+  //                 "SerumCholesterol",
+  //                 "SerumIron",
+  //                 "SerumMagnesium",
+  //                 "SerumProtein",
+  //                 "Sex" ,
+  //                 "SystolicBP" ,
+  //                 "TIBC" ,
+  //                 "TS(TransferrinSaturation)",
+  //                 "Whitebloodcells",
+  //                 "BMI",
+  //                 "Pulse pressure"]
+
+  request.get(
+    {
+      url: "http://127.0.0.1:5000/riskOfDeath",
       body: req.body,
       json: true,
     },
--- src/ml/test.py
+++ src/ml/test.py
@@ -8,6 +8,8 @@ sys.path.insert(0, r'utils')
 
 from utils import Xray_util as util
 from utils import Retinopathy_util as retinopathy
+from utils import RiskOfDeath_util as riskOfDeath
+
 
 import numpy as np
 
@@ -39,9 +41,12 @@ def flask():
 @app.route('/chestXray', methods=['GET'])
 def xray():
     # load model
+
+    file = request.query_string.decode('utf-8')
+    
     model = util.load_model('ML_models/Xray_model', compile=False)
 
-    buffer = util.compute_gradcam(model, 'cardiomegaly.png', IMAGE_DIR, labels, to_show)
+    buffer = util.compute_gradcam(model, file, IMAGE_DIR, labels, to_show)
 
     # base = BytesIO(base64.decodebytes(buffer))
     # img = Image.open(base)
@@ -58,12 +63,26 @@ def Retinopathy():
     res = retinopathy.predict(data)
 
     if(res==1):
-        response = "You have been diagonsed with Retinopathy"
+        response = "The Patient has been diagonsed with Diabetic Retinopathy"
     else : 
-        response = "You have not been diagonsed with Diabetic Retinopathy"
+        response = "The Patient has not been diagonsed with Diabetic Retinopathy"
 
     return response
 
+@app.route('/riskOfDeath', methods=['GET'])
+def RiskOfDeath():
+    json  = request.get_json() 
+
+    data = json['user']
+
+    res = riskOfDeath.predict(data)
+
+    if(res==1):
+        response = "The Patient is at Risk of Death"
+    else : 
+        response = "The Patient is not at Risk of Death"
+
+    return response
     
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
