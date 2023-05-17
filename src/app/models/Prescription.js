29aa4fc006993e0b2d59f5cece1d30ca1c17423f
Ali Hamza <hamza120025@gmail.com>
2023-05-17 21:57:47 +0500

    changed a few stuff

--- src/app/models/Prescription.js
+++ src/app/models/Prescription.js
@@ -1,60 +1,61 @@
-const mongoose = require('mongoose');
-const { create } = require('./User');
-
-
+const mongoose = require("mongoose");
+const { create } = require("./User");
 
 const MedicineSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    dosage_form: {
-        type: String,
-        required: true
-    },
-    dosage_frequency: {
-        type: String,
-        required: true
-    },
-    days: {
-        type: Number,
-        required: true
-    },
-    additional_days: {
-        type: Number,
-        required: true
-    },
-    precautionary_details: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  dosage_form: {
+    type: String,
+    required: true,
+  },
+  dosage_frequency: {
+    type: String,
+    required: true,
+  },
+  dosage_size: {
+    type: String,
+    required: true,
+  },
+  days: {
+    type: Number,
+    required: true,
+  },
+  additional_days: {
+    type: Number,
+  },
+  precautionary_details: {
+    type: String,
+    required: true,
+  },
 });
 
 const PrescriptionSchema = mongoose.Schema({
-
-    patient: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Patient',
-        required: true
-
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Patient",
+    required: true,
+  },
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Doctor",
+    required: true,
+  },
+  medicines: [
+    {
+      type: MedicineSchema,
+      required: true,
     },
-    doctor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Doctor',
-        required: true
-    },
-    medicines: [{
-        type: MedicineSchema,
-        required: true
-    }],
-    file: {
-        type: String,
-        default: 'default.png'
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
+  ],
+  file: {
+    type: String,
+    default: "default.png",
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Prescription', PrescriptionSchema);
+module.exports = mongoose.model("Prescription", PrescriptionSchema);
