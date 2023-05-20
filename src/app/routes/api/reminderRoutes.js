2f4f70613eef814caa832ca2e0663fc9a308c159
Mohammad Haris Zia <63912301+MohammadHarisZia@users.noreply.github.com>
2023-05-21 01:41:12 +0500

    Haris (#27)
    * Added firebase notifications through backend support
    
    * Added Notifications and Xray Diagnosis with GridFS
    
    * Added Brain MRI with notifications and GridFS filing
    
    * changes in routes, notification send route and xray and brain mri diagnosis
    
    * Added notification get route
    
    * Added dockerize support for python microservice
    
    * Added bert and refactored some code in test.py of ml folder
    
    * issue when merge
    
    * changes in git ignore
    
    * Added medicine reminder
    
    * Added notification default activity to start app
    
    * Fixed issue in index.js import
    
    * Changes in reminder and added cron job

--- src/app/routes/api/reminderRoutes.js
+++ src/app/routes/api/reminderRoutes.js
@@ -3,11 +3,14 @@ const express = require("express");
 const {
   createReminder,
   getAllReminders,
+  printReminder,
 } = require("../../controllers/api/reminderController");
 
 const router = express.Router();
 
 router.route("/").post(createReminder).get(getAllReminders);
+
+router.route("/print").get(printReminder);
 
 // router.route('/group-by-months').get(getAppointmentsGroupedByMonths);
 
